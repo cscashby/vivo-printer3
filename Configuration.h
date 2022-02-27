b5778e3f479e45b159f701528a58af11367cbb28
Christian Ashby <git@cashby.me.uk>
2022-02-27 12:57:11 +0000

    Updating with better bed level speed

--- Configuration.h
+++ Configuration.h
@@ -801,7 +801,7 @@
 // The number of probes to perform at each point.
 //   Set to 2 for a fast/slow probe, using the second probe result.
 //   Set to 3 or more for slow probes, averaging the results.
-#define MULTIPLE_PROBING 2
+//#define MULTIPLE_PROBING 2
 
 /**
  * Z probes require clearance when deploying, stowing, and moving between
@@ -817,7 +817,7 @@
  * Example: `M851 Z-5` with a CLEARANCE of 4  =>  9mm from bed to nozzle.
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
-#define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
+#define Z_CLEARANCE_DEPLOY_PROBE   5 // Z Clearance for Deploy/Stow
 
 #if POWER_LOSS_RECOVER_SUPER_CAP
 #define Z_CLEARANCE_BETWEEN_PROBES  0 // Z Clearance between probe points
